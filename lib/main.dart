4b529685cc61766585aa689605c06d18eb57319c
Beyz61 <6trabzn1@gmail.com>
2024-11-04 14:52:57 +0100

    Added Timer Button 3 times in a row.

--- lib/main.dart
+++ lib/main.dart
@@ -1,18 +1,37 @@
 import 'package:flutter/material.dart';
+import 'package:ps_521_timer_app/timer_button.dart';
 
 void main() {
   runApp(const MainApp());
 }
 
-class MainApp extends StatelessWidget {
+class MainApp extends StatefulWidget {
   const MainApp({super.key});
 
   @override
+  State<MainApp> createState() => _MainAppState();
+}
+
+class _MainAppState extends State<MainApp> {
+  @override
   Widget build(BuildContext context) {
-    return const MaterialApp(
+    return MaterialApp(
       home: Scaffold(
-        body: Center(
-          child: Text('Hello World!'),
+        appBar: AppBar (title: const Text("Stopwatch", style: TextStyle(
+          color:Colors.white, fontSize: 20, fontWeight: FontWeight.bold),),
+         backgroundColor: const Color.fromARGB(255, 9, 49, 81),
+        ),
+        body: const Center(
+          child: Row(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              TimerButton(text: "Start"),
+              SizedBox(width: 10), 
+              TimerButton(text: "Stop"),
+              SizedBox(width: 10),
+              TimerButton(text: "Clear"),
+            ],
+          ),
         ),
       ),
     );
